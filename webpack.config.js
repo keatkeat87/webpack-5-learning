e1f3cc7bf477ce63d53c5bc3b5e17117a57e9720
Derrick Yam <keatkeat87@live.com>
2021-10-08 21:43:29 +0800

    before ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,18 @@ const pathHelper = require("path");
 module.exports = {
   mode: "production",
   entry: {
-    index: "./index.js",
+    index: "./index.ts",
   },
   output: {
     path: pathHelper.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
 };
